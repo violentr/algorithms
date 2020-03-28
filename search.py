14288eacb2626bd0c51e76533c4110541445ea60
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-03-28 16:35:52 +0000

    Add binary search algo

---
+++ search.py
@@ -0,0 +1,26 @@
+def binary_search(array, search_for):
+    slice_start = 0
+    slice_end = len(l) - 1
+    found = False
+    location = ''
+    while slice_start <= slice_end and not found:
+        print("Slice end", slice_end)
+        location = (slice_start + slice_end)
+        if array[location] == search_for:
+            found = True
+        else:
+            if search_for < array[location]:
+                print("slice end", slice_end)
+                slice_end = location - 1
+            else:
+                print("slice start", slice_start)
+                slice_start = location + 1
+    return location
+
+print("\n")
+print("=============================")
+l = [2, 3, 5, 8, 11, 12, 18]
+number = 11
+result = binary_search(l, number)
+
+print("Binary Search: number %d found at position %d" % (number, result))
